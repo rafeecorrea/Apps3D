596ff4ef8867628a095b8aa8b134d5e4641e1bf6
rafeecorrea <90203255+rafeecorrea@users.noreply.github.com>
2021-09-06 09:34:44 -0500

    Add files via upload

---
+++ js/test_1JS.js
@@ -0,0 +1,17 @@
+function start(){
+	
+	
+	const canvas=
+	document.querySelector("#glcanvas");
+	
+	
+	const gl= canvas.getContext("webgl");
+	
+	if (!gl){
+		
+		alert("El navegador no es compatible con WEBGL")
+		return;
+	}
+	gl.clearColor(1.0, 1.0, 0.0, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT)
+	}
